a7bc1487a9b55f171470b4d39372483e698ce820
subbuplatina <subbuplatina@gmail.com>
2019-04-02 08:43:08 +0530

    Added subbu changes to Dhana's First Program java class file.

--- test1/src/test1/java/firstprogram.java
+++ test1/src/test1/java/firstprogram.java
@@ -20,6 +20,8 @@ public class firstprogram {
 		String s2=Integer.toString(a2);
 		System.out.println("string1 "+s1);
 		System.out.println("string 2"+s2);
+		
+		System.out.println("This is Subbu.");
 	}
 
 }
