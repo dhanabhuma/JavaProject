5f1d8f853e641b8430aeb642968a7ab5048edc2c
Dhana Bhuma <dhanabhuma@gmail.com>
2019-04-01 22:01:34 -0500

    Adding to GIT first project repo.

---
+++ test1/src/test1/java/firstprogram.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package test1.java;
+
+/**
+ * @author Subodh
+ *
+ */
+public class firstprogram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("helloworld");
+		//String str1="merit";
+		//String str2="student";
+		// String str = str1 + str2;
+		int a1=1234;
+		int a2=-1234;
+		String s1=Integer.toString(a1);
+		String s2=Integer.toString(a2);
+		System.out.println("string1 "+s1);
+		System.out.println("string 2"+s2);
+		
+		
+		//System.out.print(+str1);
+		// TODO Auto-generated method stub
+
+	}
+
+}
